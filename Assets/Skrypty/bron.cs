fa7e677c24d30e9161877a23ee0a0b99a1728ddc
paknot <73398335+paknot@users.noreply.github.com>
2021-02-13 17:42:48 +0100

    ,

---
+++ Assets/Skrypty/bron.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class bron : MonoBehaviour
+{
+    public float obrazenia = 10f;
+    public float zasieg = 100f;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+
+        void Shoot () {
+            RaycastHit hit;
+           if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, zasieg)) 
+           {
+            Debug.Log(hit.transform.name);
+
+            target cel = hit.transform.GetComponent<target>();
+            if(cel != null)
+                {
+                    cel.TakeDamage(obrazenia);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/Skrypty/target.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public float zdrowie = 50f;
+    public void TakeDamage (float amount)
+    {
+        zdrowie -=amount;
+        if(zdrowie <=0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
