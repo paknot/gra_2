18f3ea6a3729b3bbb6a9c11a143fc575a140989c
paknot <73398335+paknot@users.noreply.github.com>
2021-04-16 14:20:53 +0200

    sas
    sasda

---
+++ Assets/Skrypty/Celowanie.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Celowanie : MonoBehaviour
+{
+
+    public Vector3 aimDownSight;
+    public Vector3 hipFire;
+  
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            transform.localPosition = Vector3.Slerp(transform.localPosition,aimDownSight, 10 * Time.deltaTime);
+        }
+        if(Input.GetMouseButtonUp(1))
+        {
+            transform.localPosition = hipFire;
+        }
+    }
+}
